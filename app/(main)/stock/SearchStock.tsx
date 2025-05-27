1115ab7fe47abdc19092f07cd6a10d565a140764
kwgon0212 <kwgon0102@gmail.com>
2025-05-27 21:28:31 +0900

    feat: 종목 검색 모달 UI 구현

--- app/(main)/stock/SearchStock.tsx
+++ app/(main)/stock/SearchStock.tsx
@@ -1,13 +1,18 @@
 "use client";
 
+import Modal from "@/components/ui/Modal";
 import Input from "@/components/ui/shared/Input";
 import { useDebounce } from "@/hooks/useDebounce";
-import { Search } from "lucide-react";
+import { ChevronRight, Search } from "lucide-react";
+import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 
 const SearchStock = () => {
+  const router = useRouter();
   const [stockSearch, setStockSearch] = useState("");
   const debouncedSearch = useDebounce(stockSearch, 500);
+  const [isOpen, setIsOpen] = useState(false);
+  const [searchResult, setSearchResult] = useState<any[]>([]);
 
   useEffect(() => {
     if (debouncedSearch) {
@@ -15,27 +20,86 @@ const SearchStock = () => {
     }
   }, [debouncedSearch]);
 
-  console.log(debouncedSearch);
+  useEffect(() => {
+    if (stockSearch === "") {
+      setSearchResult([]);
+    }
+  }, [stockSearch]);
 
   const searchStocks = async (query: string) => {
     // API 호출 등 검색 로직
-    console.log("Searching for:", query);
+    setSearchResult([query, query]);
   };
   return (
-    <div className="w-full shadow-color rounded-main relative">
-      <Input
-        value={stockSearch}
-        onChange={(e) => setStockSearch(e.target.value)}
-        placeholder="종목명 또는 코드 검색"
-        width="full"
-      />
-      <Search className="absolute right-main top-[50%] translate-y-[-50%]" />
-      {debouncedSearch && (
-        <div className="w-full absolute left-0 pt-main">
-          <div className=" bg-main-blue rounded-main"></div>
+    <>
+      <div className="w-full shadow-color rounded-main relative">
+        <Input
+          placeholder="종목명 또는 코드 검색"
+          width="full"
+          onClick={() => setIsOpen(true)}
+        />
+        <Search className="absolute right-main top-[50%] translate-y-[-50%]" />
+      </div>
+      <Modal
+        isOpen={isOpen}
+        onClose={() => {
+          setIsOpen(false);
+          setStockSearch("");
+          setSearchResult([]);
+        }}
+        hasCloseButton={false}
+      >
+        <div className="w-[500px] flex flex-col gap-main">
+          <div className="w-full shadow-color rounded-main relative">
+            <Input
+              ref={(el) => {
+                if (el && isOpen) {
+                  el.focus();
+                }
+              }}
+              value={stockSearch}
+              onChange={(e) => setStockSearch(e.target.value)}
+              placeholder="종목명 또는 코드 검색"
+              width={400}
+              onClick={() => setIsOpen(true)}
+            />
+            <Search className="absolute right-main top-[50%] translate-y-[-50%]" />
+          </div>
+
+          {stockSearch && searchResult.length === 0 ? (
+            <div className="w-full h-[200px] flex items-center justify-center">
+              <p className="text-main-dark-gray">검색 결과가 없습니다.</p>
+            </div>
+          ) : (
+            <>
+              {searchResult.map((result, idx) => (
+                <div
+                  className="w-full flex flex-col justify-around hover:bg-main-blue/10 rounded-main transition-colors duration-200 ease-in-out p-main gap-[5px] group relative"
+                  key={`search-stock-${result}-${idx}`}
+                  onClick={() => router.push(`/stock/${result}`)}
+                >
+                  <div className="flex items-center gap-2 w-full">
+                    <div className="bg-black rounded-full size-[40px] shrink-0" />
+                    <div className="flex flex-col flex-1 truncate text-sm">
+                      <span className="font-bold text-gray-800 truncate w-full">
+                        포스코홀딩스
+                      </span>
+                      <span className="text-main-dark-gray">{result}</span>
+                    </div>
+                  </div>
+                  <button className="absolute top-1/2 -translate-y-1/2 right-main hidden group-hover:block">
+                    <ChevronRight
+                      className="text-main-blue hover:bg-main-blue/30 rounded-full p-1 box-content transition-colors duration-200 ease-in-out"
+                      size={20}
+                    />
+                  </button>
+                </div>
+              ))}
+            </>
+          )}
         </div>
-      )}
-    </div>
+      </Modal>
+    </>
   );
 };
 
