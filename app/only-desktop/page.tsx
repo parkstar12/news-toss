65c97c4fb6ebf2e739b90c1796f88190c2038e9d
kwgon0212 <kwgon0102@gmail.com>
2025-05-27 21:49:28 +0900

    feat: 잘못된 종목 상세페이지 이동 시 리다이렉트 및 모바일 기기 접속 방지

--- app/only-desktop/page.tsx
+++ app/only-desktop/page.tsx
@@ -1,24 +1,32 @@
 import React from "react";
-import { Monitor } from "lucide-react";
-// import Image from "next/image";
-// import newsTossLogo from "@/public/news-toss-logo.png";
+import Image from "next/image";
+import newsTossLogo from "@/public/news-toss-logo.png";
 
-export default function OnlyDesktopPage() {
+const OnlyDesktopPage = () => {
   return (
-    <div className="w-full h-screen overflow-hidden flex flex-col items-center justify-center">
-      <Monitor size={64} className="mb-6 text-main-blue" />
-      <h1 className="text-2xl font-bold text-main-dark-gray mb-2">
+    <div className="w-full h-screen overflow-y-hidden flex flex-col items-center justify-center gap-[20px]">
+      <Image
+        src={newsTossLogo}
+        alt="news-toss-logo"
+        width="100"
+        height="100"
+        className="rounded-full"
+      />
+
+      <h1 className="text-2xl font-bold text-main-dark-gray">
         데스크탑에서 접속해 주세요
       </h1>
 
-      {/* <Image src={newsTossLogo} alt="news-toss-logo" width="100" height="100" /> */}
-
-      <p className="text-main-dark-gray text-base text-center mb-2">
-        PC(데스크탑) 환경에서만 이용하실 수 있습니다.
-      </p>
-      <p className="text-main-dark-gray text-sm text-center">
-        더 넓은 화면에서 쾌적하게 서비스를 이용해 주세요.
-      </p>
+      <div>
+        <p className="text-main-dark-gray text-base text-center mb-2">
+          PC(데스크탑) 환경에서만 이용하실 수 있습니다.
+        </p>
+        <p className="text-main-dark-gray text-sm text-center">
+          더 넓은 화면에서 쾌적하게 서비스를 이용해 주세요.
+        </p>
+      </div>
     </div>
   );
-}
+};
+
+export default OnlyDesktopPage;
--- middleware.ts
+++ middleware.ts
@@ -13,13 +13,29 @@ export function middleware(req: NextRequest) {
     return NextResponse.redirect(new URL("/only-desktop", req.url));
   }
 
-  // if (request.nextUrl.pathname === "/only-desktop") {
-  //   return NextResponse.redirect(new URL("/home", request.url));
-  // }
+  if (pathname.startsWith("/stock/")) {
+    // /stock/ 다음의 code 추출
+    const code = pathname.split("/")[2];
+
+    // code가 6자리 숫자가 아니면 리다이렉트
+    if (!/^\d{6}$/.test(code || "")) {
+      return NextResponse.redirect(new URL("/stock", req.url));
+    }
+  }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: "/:path*",
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+    "/stock/:path*",
+  ],
 };
