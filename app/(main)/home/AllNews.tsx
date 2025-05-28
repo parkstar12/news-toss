f273bacbc48bff88ee72a06cbaa65bbc3aabcbaa
kwgon0212 <kwgon0102@gmail.com>
2025-05-28 09:40:18 +0900

    chore: 메인 뉴스페이지 모든 주요 뉴스 더보기 클릭 시 더미데이터 추가

--- app/(main)/home/AllNews.tsx
+++ app/(main)/home/AllNews.tsx
@@ -6,12 +6,32 @@ import NewsModal from "./NewsModal";
 
 const AllNews = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [newsList, setNewsList] = useState(
+    Array.from({ length: 9 }).map((_, index) => ({
+      title: "주요 기업들, 2분기 실적",
+      time: "5시간 전",
+      source: "이투데이",
+    }))
+  );
+
+  const handleMoreNews = () => {
+    setNewsList(
+      newsList.concat(
+        Array.from({ length: 9 }).map((_, index) => ({
+          title: "주요 기업들, 2분기 실적",
+          time: "5시간 전",
+          source: "이투데이",
+        }))
+      )
+    );
+  };
+
   return (
     <>
       <div className="flex flex-col gap-main">
         <h2 className="text-2xl font-bold">모든 주요 뉴스</h2>
         <div className="grid grid-cols-3 gap-main">
-          {Array.from({ length: 9 }).map((_, index) => (
+          {newsList.map((news, index) => (
             <button
               className="flex gap-main hover:scale-102 transition-all duration-500 ease-in-out"
               key={`all-news-${index}`}
@@ -19,14 +39,11 @@ const AllNews = () => {
             >
               <div className="bg-black size-[70px] rounded-main shrink-0" />
               <div className="w-full flex flex-col justify-around">
-                <p className="line-clamp-1">
-                  주요 기업들, 2분기
-                  실적adfaefaefaㅁ이라ㅓ미다러믿러aroirajho;ijego;
-                </p>
+                <p className="line-clamp-1 text-start">{news.title}</p>
                 <div className="flex items-center text-main-dark-gray text-xs">
                   <Clock className="h-3 w-3 mr-1 text-main-dark-gray" />
                   <span className="text-main-dark-gray">
-                    5시간 전 · 이투데이
+                    {news.time} · {news.source}
                   </span>
                 </div>
               </div>
@@ -34,14 +51,17 @@ const AllNews = () => {
           ))}
         </div>
         <div className="flex justify-center border-t border-gray-200 pt-main">
-          <button className="text-main-dark-gray text-sm hover:bg-main-light-gray w-full rounded-main py-main transition-all duration-300 ease-in-out">
+          <button
+            className="text-main-dark-gray text-sm hover:bg-main-light-gray w-full rounded-main py-main transition-all duration-300 ease-in-out"
+            onClick={handleMoreNews}
+          >
             더보기
           </button>
         </div>
       </div>
 
       <NewsModal isOpen={isOpen} onClose={() => setIsOpen(false)}>
-        1
+        <></>
       </NewsModal>
     </>
   );
